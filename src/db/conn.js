5168e0085b39d002f5ce3bd4031eb9b43eebcfad
gabs38670 <gabriel.frodrigues15@senacsp.edu.br>
2024-06-14 14:09:18 -0300

    criando front do cartao lendMoney

--- src/db/conn.js
+++ src/db/conn.js
@@ -11,7 +11,7 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('LendMoney2', 'root', 'root', {
+const sequelize = new Sequelize('LendMoney2', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
