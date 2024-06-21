f8437062fca18ff2293a155643d384382774e3e1
gabs38670 <gabriel.frodrigues15@senacsp.edu.br>
2024-06-21 15:09:27 -0300

    criando emprestimo de solicitação do emprestador

---
+++ src/controllers/solicitar.js
@@ -0,0 +1,39 @@
+const express = require ('express')
+const db = require('../db/conn')
+const Emprestimo_solicitado = require('../models/emprestimo_solicitado')
+const solicitadores = require('../models/solicitador')
+
+
+module.exports = class solicitar {
+    static  emprestimo (req ,res){
+       res.render ('emprestimo', {isAuthenticated: req.session.isAuthenticated})  
+    }
+    
+
+     static emprestimo (req,res){
+         solicitadores.findAll ({raw: true, limit:1})
+          .then((data) =>{
+             res.render('emprestimo',{solicitadores: data})
+           })
+           .catch((err) => console.log(err))  
+     }
+  
+    static emprestimoSave (req,res){
+        const emprestimoSave = {
+            valor: req.body.valor,
+            id_solicitador: req.body.id_solicitador,
+            DataMAX_Devolucao: req.body.DataMAX_Devolucao,
+            DataDaSolicitacao: req.body.DataDaSolicitacao,
+        }
+       
+        Emprestimo_solicitado.create(emprestimoSave)
+             .then(res.redirect('/emprestimo'))
+            .catch((err)=> console.log()) 
+} 
+
+}
+
+
+
+
+
